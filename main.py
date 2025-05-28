49ff5148a6f845a2a628fa77a624c8160fb61c79
shimoyama <s2257234@g.tohoku-gakuin.ac.jp>
2025-05-29 00:29:14 +0900

    gymを使わない環境＋順位表示

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from game.player import Player
-from game.game import Game
-
+from game.environment import DaifugoSimpleEnv
 
 def main():
-    players = [Player(i) for i in range(4)]
-    game = Game(players)
-    game.play_game()
-    for pid, action in game.history:
-        print(f"Player {pid} played: {action}")
+    env = DaifugoSimpleEnv(num_players=4)
+    obs = env.reset()
+    env.render()
+
+    while not env.done:
+        #input("Enter を押すと次のターンに進みます...")
+        obs, reward, done = env.step()
+        env.render()
+        if done:
+            print("🎉 ゲーム終了！")
+            print("🏆 順位：")
+            for i, player_idx in enumerate(env.game.rankings):
+                print(f"{i+1}位: Player {player_idx}")
+                break
 
 if __name__ == "__main__":
     main()
